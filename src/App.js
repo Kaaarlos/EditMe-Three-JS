5fd9494f203cf91b84d5b4c77e2ead6d4031c667
Carlos Alberto Ceja Zapata <73798412+Kaaarlos@users.noreply.github.com>
2022-08-29 12:08:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Wrapper2 from "./components/Wrapper2";
 import Wrapper3 from "./components/Wrapper3";
-//import Wrapper4 from "./components/Wrapper4";
 const shoe = "/shoe2.gltf";
 const arbol = "/Arbol.gltf";
 const astro = "/Astronaut.gltf";
-//const gato = "/Gato.gltf";
+
 
 function App() {
   return (
@@ -18,11 +17,6 @@ function App() {
           astro: astro,
         }}
       />
-      /*<Wrapper4
-        customGato={{
-          gato: gato,
-        }}
-      />*/
       <Wrapper
         customShoes={{
           shoe: shoe,
